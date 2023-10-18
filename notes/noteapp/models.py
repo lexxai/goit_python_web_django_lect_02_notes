3a1179a98b5fcc34eb7970e20c21868b2fc1f1be
lexxai <lexxai@users.noreply.github.com>
2023-10-18 03:15:19 +0300

    users, reamde

--- notes/noteapp/models.py
+++ notes/noteapp/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=25, null=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "name"], name="tag of username")
+        ]
 
     def __str__(self) -> str:
         return f"{self.name}"
-    
+
 
 class Note(models.Model):
     name = models.CharField(max_length=50, null=False)
@@ -15,8 +23,7 @@ class Note(models.Model):
     done = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self) -> str:
         return f"{self.name}"
-
-    
